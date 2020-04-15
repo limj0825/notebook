0815ad8973574c5bc77ba2691ec1633a4e9bf278
Mr.Lee <limj0825@foxmail.com>
2020-04-15 22:03:43 +0800

    add bg.sh

---
+++ scripts/bg.sh
@@ -0,0 +1,1 @@
+jupyter notebook --NotebookApp.tornado_settings='{"headers":{"Content-Security-Policy":"frame-ancestors self http://localhost:3000 http://localhost:3001 http://kfcoding.com http://gitcourse.kfcoding.com;" }}'
