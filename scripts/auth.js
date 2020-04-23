f30ebc166a3f4f13ad1763a51e0a4524a588f6c4
Mr.Lee <limj0825@foxmail.com>
2020-04-23 17:47:08 +0800

    change course.json

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,25 +1,25 @@
 function getCookie(name) {
-	var value = `; $(document.cookie}`;
-	var pairs = value.split("; " + name + "=");
-	if (pairs.length == 2) {
-		return pairs.pop().split(";").shift()
-	};
+    var value = `; ${document.cookie}`;
+    var pairs = value.split("; " + name + "=");
+    if (pairs.length == 2) {
+        return pairs.pop().split(";").shift()
+    };
 }
-
-if (document.location.host == `gitcourst.kfcoding.com`) {
-	var token=getCookie('token');
-	fetch('http://api.kfcoing.com/api/basic/user/current', {
-		headers: {
-			'Content-Type': 'application/json',
-			'Authorization': token
-		},
-		method: 'GET',
-	}).then(response => respone.json()).then(data => {
-		if (data.status !== 200) {
-			window.location.href = "http://kfcoding.com/user/login?redirect=" + window.location.href;
-		}
-		else {
-			window.env=[`token=${token}`]
-		}
-	});
+  
+if (document.location.host == 'gitcourse.kfcoding.com') {
+    var token=getCookie('token');
+    fetch('http://api.kfcoding.com/api/basic/users/current', {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': token
+        },
+        method: 'GET',
+    }).then(response => response.json()).then(data => {
+        if (data.status !== 200) {
+            window.location.href = "http://kfcoding.com/user/login?redirect=" + window.location.href;
+        }
+        else{
+            window.env=[`token=${token}`]
+        }
+    });
 }
